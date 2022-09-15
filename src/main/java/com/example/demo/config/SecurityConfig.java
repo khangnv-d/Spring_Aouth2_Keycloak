e8be39b43e96d1c804a4abda71c2a4014b7bf908
Nguyen Van Khang <nguyenvankhang0211@gmail.com>
2022-09-15 17:43:49 +0700

    init source code

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+//package com.example.demo.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                //.csrf().disable()
+//                .authorizeHttpRequests( (request) -> request
+//                        .antMatchers("/").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+////                .formLogin((form) -> form
+////                        .loginPage("/login")
+////                        .permitAll()
+////                )
+//                .logout((logout) -> logout.permitAll());
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user =
+//                User.withDefaultPasswordEncoder()
+//                        .username("user")
+//                        .password("password")
+//                        .roles("USER")
+//                        .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
+//
+////    @Bean
+////    public WebSecurityCustomizer webSecurityCustomizer() {
+////        return (web) -> web.ignoring().antMatchers("/api/test");
+////    }
+//}
