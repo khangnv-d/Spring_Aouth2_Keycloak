da08817119a274d1cd73df8e9a5cb9d7d7161cfa
Nguyen Van Khang <nguyenvankhang0211@gmail.com>
2022-09-16 17:40:21 +0700

    fix conflict

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,48 +1,43 @@
 package com.example.demo.controller;
 
+import com.example.demo.config.KeycloakClientConfig;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/api")
 @RequiredArgsConstructor
 public class HomeController {
 
-    @Autowired
-    private OAuth2AuthorizedClientService authorizedClientService;
-
-    private OAuth2AuthorizedClient getAuthorizedClient(OAuth2AuthenticationToken authentication) {
-        return this.authorizedClientService.loadAuthorizedClient(
-                authentication.getAuthorizedClientRegistrationId(), authentication.getName());
-    }
-
-    @GetMapping({"/", "index"})
-    public String getIndex(Model model,
-                           @AuthenticationPrincipal OAuth2User principal,
-                           OAuth2AuthenticationToken authentication) {
-
-        OAuth2AuthorizedClient authorizedClient = this.getAuthorizedClient(authentication);
-
-        //authentication.getPrincipal().getAttributes().forEach((k, v) -> System.out.println("key: " + k + " value:" + v));
-        model.addAttribute("userName", authentication.getPrincipal().getAttribute("preferred_username"));
-
-        //authorizedClient.getAccessToken().getScopes().stream().forEach(System.out::println);
-        //System.out.println("authentication." + authorizedClient.getClientRegistration());
-        model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientId());
-        model.addAttribute("scopes", authorizedClient.getAccessToken().getScopes());
-
-        return "Index";
-    }
+    private final KeycloakClientConfig keycloakClient;
+//    @Autowired
+//    private OAuth2AuthorizedClientService authorizedClientService;
+//
+//    private OAuth2AuthorizedClient getAuthorizedClient(OAuth2AuthenticationToken authentication) {
+//        return this.authorizedClientService.loadAuthorizedClient(
+//                authentication.getAuthorizedClientRegistrationId(), authentication.getName());
+//    }
+//
+//    @GetMapping({"/", "index"})
+//    public String getIndex(Model model,
+//                           @AuthenticationPrincipal OAuth2User principal,
+//                           OAuth2AuthenticationToken authentication) {
+//
+//        OAuth2AuthorizedClient authorizedClient = this.getAuthorizedClient(authentication);
+//
+//        //authentication.getPrincipal().getAttributes().forEach((k, v) -> System.out.println("key: " + k + " value:" + v));
+//        model.addAttribute("userName", authentication.getPrincipal().getAttribute("preferred_username"));
+//
+//        //authorizedClient.getAccessToken().getScopes().stream().forEach(System.out::println);
+//        //System.out.println("authentication." + authorizedClient.getClientRegistration());
+//        model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientId());
+//        model.addAttribute("scopes", authorizedClient.getAccessToken().getScopes());
+//        //Keycloak keycloak = keycloakClient.keycloak();
+//        String a = "a";
+//
+//        return "Index";
+//    }
 
     @GetMapping("/test")
     public String test() {
---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.example.demo.controller;
+
+import com.example.demo.role.RoleService;
+import com.example.demo.user.User;
+import com.example.demo.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @GetMapping
+    public List<UserRepresentation> findAll() {
+        return userService.findAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserRepresentation findById(@PathVariable String id) {
+        return userService.findById(id);
+    }
+
+    @GetMapping("/username/{username}")
+    public List<UserRepresentation> findByUsername(@PathVariable String username) {
+        return userService.findByUsername(username);
+    }
+
+    @PostMapping
+    public ResponseEntity<URI> createUser(@RequestBody User newUser) {
+        Response response = userService.createUser(newUser);
+
+        if (response.getStatus() != 201)
+            throw new RuntimeException("User was not created");
+
+        return ResponseEntity.created(response.getLocation()).build();
+    }
+
+    public void assignRole(@PathVariable String userId,
+                           @PathVariable String roleName) {
+        RoleRepresentation role = roleService.findRoleByName(roleName);
+        userService.assignRole(userId, role);
+    }
+
+}
